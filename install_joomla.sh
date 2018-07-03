30b2bf6accfff910d16456a2564a20ff1c72a502
Sonny <s0nn46a13@users.noreply.github.com>
2018-07-03 14:33:53 -0500

    Create install_joomla.sh

---
+++ install_joomla.sh
@@ -0,0 +1,63 @@
+# Update OS
+sudo yum -y update
+
+# Install common tools
+sudo yum -y install open-vm-tools yum-utils nano wget git mlocate gzip
+sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum-config-manager --enable epel
+
+# Install Apache, disable SELinux, and open ports
+sudo yum -y install httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+sudo firewall-cmd --permanent --zone=public --add-service=http
+sudo firewall-cmd --permanent --zone=public --add-service=https
+sudo firewall-cmd --reload
+
+# Install PHP 7.2
+sudo yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+sudo yum-config-manager --enable remi-php72
+sudo yum -y install php php-mbstring php-gd php-xml php-pear php-fpm php-mysql php-pdo php-opcache
+
+# Install MariaDB
+sudo cat << EOF >/etc/yum.repos.d/MariaDB.repo
+[mariadb]
+name = MariaDB
+baseurl = http://yum.mariadb.org/10.1/centos7-amd64
+gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
+gpgcheck=1
+EOF
+sudo yum -y install mariadb mariadb-server mariadb-devel MariaDB-shared
+
+# Start, configure, & create DB
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+sudo mysql_secure_installation << EOF
+
+y
+Joomla!3.8DB
+Joomla!3.8DB
+y
+y
+y
+y
+EOF
+sudo mysql -u root -p << EOF
+Joomla!3.8DB
+create database joomladb;
+create user joomla@localhost identified by 'Joomla!3.8DB';
+grant all on joomladb.* to joomla@localhost;
+flush privileges;
+exit
+EOF
+
+# Install Joomla
+sudo wget -c https://downloads.joomla.org/cms/joomla3/3-8-10/Joomla_3-8-10-Stable-Full_Package.tar.gz
+sudo mkdir /var/www/html/joomla
+sudo tar -zxvf Joomla_3-8-10-Stable-Full_Package.tar.gz  -C /var/www/html/joomla
+chown -R apache:apache /var/www/html/joomla
+sudo chcon -R -t httpd_sys_content_rw_t /var/www/html/joomla # Set SELinux rule for Joomla folder
+
+# Configure Web UI
+# Go to http://192.168.0.58/joomla
