74c06b7fc286646e8f8885dedc838228318cfa3c
Sonny <s0nn46a13@users.noreply.github.com>
2018-09-14 08:27:04 -0500

    Update install_snipe-IT.sh

--- install_snipe-IT.sh
+++ install_snipe-IT.sh
@@ -1,10 +1,30 @@
-# Update OS
-sudo yum -y update
+#   Do not run this as root!
+#   Do not run this as root!
+#   Do not run this as root!
+#   Do not run this as root!
 
 # Install common tools
-sudo yum -y install open-vm-tools yum-utils nano wget git mlocate
 sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum-config-manager --enable epel
+sudo yum -y install open-vm-tools yum-utils nano wget git mlocate yum-cron powershell
+
+# Install PowerShell Core
+curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
+sudo yum -y install powershell
+
+# Setup yum-cron to automatically run updates
+sudo sed -i 's/update_cmd = default/update_cmd = security/g' /etc/yum/yum-cron.conf
+sudo sed -i 's/apply_updates = no/apply updates = yes/g' /etc/yum/yum-cron.conf
+sudo sed -i 's/emit_via = stdio/emit_via = email/g' /etc/yum/yum-cron.conf
+sudo sed -i 's/email_from = root/email_from = noreply/g' /etc/yum/yum-cron.conf
+sudo sed -i 's/email_to = root/email_to = scciadit@teamscci.com/g' /etc/yum/yum-cron.conf
+sudo sed -i 's/email_host = localhost/email_host = ilsausmail.teamscci.local/g' /etc/yum/yum-cron.conf
+sudo systemctl start yum-cron
+sudo systemctl enable yum-cron
+
+# Upgrade OS
+sudo yum clean all
+sudo yum -y upgrade
 
 # Install Apache, disable SELinux, and open ports
 sudo yum -y install httpd
@@ -89,9 +109,7 @@ cd /var/www/snipe-it
 curl -sS https://getcomposer.org/installer | sudo php
 php /var/www/snipe-it/composer.phar install --no-dev --prefer-source
 
-sudo php artisan key:generate << EOF
-yes
-EOF
+sudo php artisan key:generate --force --no-interaction
 
 # Change <HOST>.<LOCALDOMAIN>.<DOMAIN> i.e. snipeit.name.com
 sudo cat << EOF >/etc/httpd/conf.d/<HOST>.<LOCALDOMAIN>.<DOMAIN>.conf
