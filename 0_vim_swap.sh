26ebd7edb2c780410ed2b96552736c37343eedbc
Sonny <s0nn46a13@users.noreply.github.com>
2018-10-22 09:39:26 -0500

    Update 0_vim_swap.sh

--- 0_vim_swap.sh
+++ 0_vim_swap.sh
@@ -4,4 +4,16 @@ yum remove -y vim-minimal
 yum install -y vim-enhanced
 yum install -y sudo
 
+# Enable colorscheme torte for user
+sudo cat << EOF >.vimrc
+syntax on
+colorscheme torte
+EOF
+
+# Enable colorscheme torte for root
+sudo cat << EOF >/root/.vimrc
+syntax on
+colorscheme torte
+EOF
+
 sh base.sh
